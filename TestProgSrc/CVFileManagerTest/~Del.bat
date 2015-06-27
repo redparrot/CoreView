4e214770394392a39913c0623e348ec52df3e2dc
redparrot <wanghao8888@outlook.com>
2015-06-27 11:17:20 +0800

    Revert "加入CVMimicDefineCommdll_ImTest和CVFileManagerTest两个测试"
    This reverts commit ce58b3cfacd0a8a614b4d7e24b53cef1162d3f8f.

--- TestProgSrc/CVFileManagerTest/~Del.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for /r /d %%i in (Debug Release) do (del /q %%i
-rd %%i)
-for /f "delims=" %%i in ('dir/b/s/a-d *.user') do (del %%i)
-for /f "delims=" %%i in ('dir/b/s/a-d *.ncb') do (del %%i)
